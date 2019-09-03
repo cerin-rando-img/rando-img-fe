41668d798c984a1f785573f8b6c4d32b327125ba
Carlos Us Santiago <cdus12@gmail.com>
2019-09-03 09:54:46 -0700

    set empty pokemonReducer and combineReducer

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducer } from 'redux';
+import images from './pokemonReducer';
+
+export default combineReducer({
+  images
+});
---
+++ src/reducers/pokemonReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  images: [],
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
